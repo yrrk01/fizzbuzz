2c44be77494c79214771429e429dce4e62c8df3c
yrrk01 <yk843295@dal.ca>
2022-09-22 13:54:07 -0300

    First Commit

---
+++ task0.py
@@ -0,0 +1,12 @@
+
+i=1
+while i <101:
+    if (i % 3 == 0):
+        print("Fizz")
+    elif(i % 5 == 0):
+        print("Buzz")
+    elif(i % 3 == 0 and i % 5 == 0 ):
+        print("FizzBuzz")
+    else:
+        print(i)
+    i=i+1
